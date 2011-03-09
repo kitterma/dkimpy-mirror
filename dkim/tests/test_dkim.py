58525fbbd709bfc400d9e9acfea479fea027b939
William Grant <me@williamgrant.id.au>
2011-03-09 20:10:18 +1100

    Add basic end-to-end signing and verification tests.

--- dkim/tests/test_dkim.py
+++ dkim/tests/test_dkim.py
@@ -1,6 +1,16 @@
+import os.path
 import unittest
 
 import dkim
+
+
+def read_test_data(filename):
+    """Get the content of the given test data file.
+
+    The files live in dkim/tests/data.
+    """
+    path = os.path.join(os.path.dirname(__file__), 'data', filename)
+    return open(path).read()
 
 
 class TestFold(unittest.TestCase):
@@ -14,6 +24,30 @@ class TestFold(unittest.TestCase):
         # test.
         self.assertEqual(
             "foo"*24 + "\r\n foo", dkim.fold("foo" * 25))
+
+
+class TestSignAndVerify(unittest.TestCase):
+    """End-to-end signature and verification tests."""
+
+    def setUp(self):
+        self.message = read_test_data("test.message")
+        self.key = read_test_data("test.private")
+
+    def dnsfunc(self, domain):
+        self.assertEqual('test._domainkey.example.com.', domain)
+        return read_test_data("test.txt")
+
+    def test_verifies(self):
+        # A message verifies after being signed.
+        sig = dkim.sign(self.message, "test", "example.com", self.key)
+        res = dkim.verify(sig + self.message, dnsfunc=self.dnsfunc)
+        self.assertTrue(res)
+
+    def test_altered_body_fails(self):
+        # An altered body fails verification.
+        sig = dkim.sign(self.message, "test", "example.com", self.key)
+        res = dkim.verify(sig + self.message + "foo", dnsfunc=self.dnsfunc)
+        self.assertFalse(res)
 
 
 if __name__ == '__main__':
