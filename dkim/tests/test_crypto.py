88915e3a01ac2e1a12372efb82ac83ae56585d26
William Grant <me@williamgrant.id.au>
2011-03-19 18:45:04 +1100

    Fix some test_crypto literals to be byteses.

--- dkim/tests/test_crypto.py
+++ dkim/tests/test_crypto.py
@@ -17,6 +17,7 @@
 # Copyright (c) 2011 William Grant <me@williamgrant.id.au>
 
 import base64
+import binascii
 import hashlib
 import unittest
 
@@ -84,25 +85,25 @@ class TestParseKeys(unittest.TestCase):
 class TestEMSA_PKCS1_v1_5(unittest.TestCase):
 
     def test_encode_sha256(self):
-        hash = hashlib.sha256('message')
+        hash = hashlib.sha256(b'message')
         self.assertEquals(
-            '\x00\x01\xff\xff\xff\xff\xff\xff\xff\xff\x00'
-            '010\x0d\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x01\x05\x00\x04 '
-            + hash.digest(),
+            b'\x00\x01\xff\xff\xff\xff\xff\xff\xff\xff\x00'
+            b'010\x0d\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x01\x05\x00\x04'
+            b' ' + hash.digest(),
             EMSA_PKCS1_v1_5_encode(hash, 62))
 
     def test_encode_sha1(self):
-        hash = hashlib.sha1('message')
+        hash = hashlib.sha1(b'message')
         self.assertEquals(
-            '\x00\x01\xff\xff\xff\xff\xff\xff\xff\xff\x00'
-            '0!0\x09\x06\x05\x2b\x0e\x03\x02\x1a\x05\x00\x04\x14'
+            b'\x00\x01\xff\xff\xff\xff\xff\xff\xff\xff\x00'
+            b'0!0\x09\x06\x05\x2b\x0e\x03\x02\x1a\x05\x00\x04\x14'
             + hash.digest(),
             EMSA_PKCS1_v1_5_encode(hash, 46))
 
     def test_encode_forbids_too_short(self):
         # PKCS#1 requires at least 8 bytes of padding, so there must be
         # at least that much space.
-        hash = hashlib.sha1('message')
+        hash = hashlib.sha1(b'message')
         self.assertRaises(
             DigestTooLargeError,
             EMSA_PKCS1_v1_5_encode, hash, 45)
@@ -110,7 +111,7 @@ class TestEMSA_PKCS1_v1_5(unittest.TestCase):
 
 class TestRSA(unittest.TestCase):
 
-    message = '0004fb'.decode('hex')
+    message = binascii.unhexlify(b'0004fb')
     modulus = 186101
     modlen = 3
     public_exponent = 907
@@ -119,7 +120,7 @@ class TestRSA(unittest.TestCase):
     def test_perform(self):
         signed = perform_rsa(
             self.message, self.private_exponent, self.modulus, self.modlen)
-        self.assertEquals('01f140'.decode('hex'), signed)
+        self.assertEquals(binascii.unhexlify(b'01f140'), signed)
 
     def test_sign_and_verify(self):
         signed = perform_rsa(
@@ -136,11 +137,11 @@ class TestRSASSA(unittest.TestCase):
         self.hash = hashlib.sha1(self.test_digest)
 
     test_digest = '0123456789abcdef0123'
-    test_signature = (
-        'cc8d3647d64dd3bc12984947a27bdfbb565041fcc9db781afb4b60d29d288d8d60de'
-        '9e1916d6f81569c3e72af442538dd6aecb50a6de9a14565fdd679c46ff7842482e15'
-        'e5aa078549621b6f12ca8cd57ecfad95b18e53581e131c6c3c7cd01cb153adeb439d'
-        '2d6ab8b215b19be0e69ef490885004a474eb26d747a219693e8c').decode('hex')
+    test_signature = binascii.unhexlify(
+        b'cc8d3647d64dd3bc12984947a27bdfbb565041fcc9db781afb4b60d29d288d8d60d'
+        b'e9e1916d6f81569c3e72af442538dd6aecb50a6de9a14565fdd679c46ff7842482e'
+        b'15e5aa078549621b6f12ca8cd57ecfad95b18e53581e131c6c3c7cd01cb153adeb4'
+        b'39d2d6ab8b215b19be0e69ef490885004a474eb26d747a219693e8c')
 
     def test_sign_and_verify(self):
         signature = RSASSA_PKCS1_v1_5_sign(
